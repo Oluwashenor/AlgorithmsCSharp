812c00d44df6e48355087e669aef69060f69825e
Adeshina <adeshiname@gmail.com>
2022-12-04 21:05:55 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace AlgorithmsCSharp
+{
+    public class Algorithms
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            List<int> ar = new List<int> { 10, 20, 20, 10, 10, 30, 50, 10, 20 };
+            int validPair = SockMerchant.sockMerchant(5, ar);
+            //Console Writeline out valid Pair
+            Console.WriteLine(validPair);
+            
+        }
+    }
+
+}
---
+++ SockMerchant.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsCSharp
+{
+    public class SockMerchant
+    {
+        public static int sockMerchant(int n, List<int> ar)
+        {
+            int pairCounter = 0;
+            var taken = new List<int>();
+            for (var i = 0; i < ar.Count; i++)
+            {
+                for (var j = i + 1; j < ar.Count; j++)
+                {
+                    if (taken.Contains(j) || taken.Contains(i))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        if (ar[i] == ar[j])
+                        {
+                            pairCounter = pairCounter + 1;
+                            taken.Add(i);
+                            taken.Add(j);
+                            break;
+                        };
+                    }
+
+                }
+            }
+            return pairCounter;
+        }
+    }
+}
