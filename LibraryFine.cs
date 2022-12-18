83080a096ed055f7d16ea5af5ab6dada2c61821a
Adeshina <adeshiname@gmail.com>
2022-12-18 04:34:00 +0100

    Library Fine

---
+++ LibraryFine.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsCSharp
+{
+    public class LibraryFine
+    {
+
+        public static void libraryFineExecutor()
+        {
+            int fine = libraryFine(9, 6, 2015, 6, 6, 2015);
+            int fine2 = libraryFine(2, 6, 2014, 7, 6, 2014);
+            Console.WriteLine(fine2);
+        }
+        protected static int libraryFine(int d1, int m1, int y1, int d2, int m2, int y2)
+        {
+            int fine = default;
+            var returnDate = new DateTime(y1, m1, d1);
+            var dueDate = new DateTime(y2, m2, d2);
+            int periodDue = (int)(returnDate - dueDate).TotalDays;
+            if (periodDue < 1) return fine;
+            else if (periodDue > 0 && returnDate.Month == dueDate.Month && returnDate.Year == dueDate.Year)
+            {
+                fine = 15 * periodDue;
+                return fine;
+            }
+            else if (returnDate.Month > dueDate.Month && returnDate.Year == dueDate.Year)
+            {
+                fine = 500 * (returnDate.Month - dueDate.Month);
+                return fine;
+            }
+            else if (returnDate.Year > dueDate.Year)
+            {
+                fine = 10000;
+                return fine;
+
+            }
+            return fine;
+        }
+
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -4,19 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
-            List<int> ar = new List<int> { 10, 20, 20, 10, 10, 30, 50, 10, 20 };
-            List<int> arr = new List<int> { 50, 49, 38, 49, 78, 36, 25, 96,
-                10, 67, 78, 58, 98, 8, 53, 1, 4, 7, 29, 6, 59, 93, 74, 3, 67,
-                47, 12, 85, 84 ,40 ,81 ,85 ,89,70, 33, 66, 6,
-                9, 13, 67 ,75 ,42, 24 ,73 ,49 ,28, 25, 5 ,86, 53, 10,
-                44 ,45, 35, 47, 11, 81, 10, 47, 16, 49, 79, 52, 89,
-                100, 36, 6, 57, 96,18, 23, 71, 11, 99, 95, 12, 78, 19,
-                16, 64, 23, 77, 7, 19, 11, 5, 81, 43, 14, 27, 11, 63,
-                57, 62, 3, 56, 50, 9, 13, 45 };
-            int validPair = SockMerchant.sockMerchant(5, arr);
-            //Console Writeline out valid Pair
-            Console.WriteLine(validPair);
+            Console.WriteLine("Hello, Algorithm World");
+
+            //SockMerchant.sockMerchantExecutor();
+            LibraryFine.libraryFineExecutor();
+
 
         }
     }
