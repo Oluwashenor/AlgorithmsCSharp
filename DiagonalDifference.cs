64116e8544622d219d9dacba928c7ff37e263c7f
Adeshina <adeshiname@gmail.com>
2022-12-30 20:33:44 +0100

    Diagonal Difference

---
+++ DiagonalDifference.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsCSharp
+{
+	public class DiagonalDifference
+	{
+		public static int diagonalDifference(List<List<int>> arr)
+		{
+			int pd = 0;
+			int sd = 0;
+			int length = arr.Count;
+			int index = 0;
+			int sdIndex = arr.Count;
+
+			while (index < length)
+			{
+				pd = pd + arr[index][index];
+				sd = sd + arr[index][sdIndex - 1];
+				index = index + 1;
+				sdIndex = sdIndex - 1;
+			}
+			return Math.Abs(pd - sd);
+		}
+
+	}
+}
+}
