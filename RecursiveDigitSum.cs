38c880235d22792afbebe2bc6c1c429de3a5a307
Adeshina <adeshiname@gmail.com>
2023-01-01 12:49:01 +0100

    Recursive Digit Sum

---
+++ RecursiveDigitSum.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsCSharp
+{
+    public class RecursiveDigitSum
+    {
+        public static int superDigit(string n, int k)
+        {
+            int superD = mySuperDigit(n, k);
+            Console.WriteLine(superD);
+            return superD;
+        }
+
+        public static int mySuperDigit(string value, int k)
+        {
+            BigInteger sum = 0;
+            for (int i = 0; i< value.Length; i++)
+            {
+                int x = k * Convert.ToInt32(value[i].ToString());
+                 sum = sum + x;  
+
+            }
+            if (sum > 10) { sum =  mySuperDigit(sum.ToString(), 1); }
+            return (int)sum;
+        }
+
+    }
+
+}
